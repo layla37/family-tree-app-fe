6829ddfd8d239cedb9c2e9c24cfe30d72d35eca1
Layla <laylamandella@gmail.com>
2022-01-04 12:08:16 -0800

    add family tree graph

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Person from './components/Person';
 import Notification from './components/Notification';
 import Footer from './components/Footer';
 import FamilyMemberForm from './components/FamilyMemberForm';
+import { FamilyTree } from './components/FamilyTree';
 import peopleRequest from './services/people';
 
 const App = () => {
   const [people, setPeople] = useState([]);
-  // const [newPersonName, setNewPersonName] = useState('');
-  // const [newPersonBio, setNewPersonBio] = useState('');
-  // const [newPersonPartner, setNewPersonPartner] = useState('');
-  // const [newPersonParents, setNewPersonParents] = useState([]);
-  // const [newPersonChildren, setNewPersonChildren] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
@@ -22,22 +19,53 @@ const App = () => {
     })
   }, []);
 
+  // const getPersonPath = (name) => {
+  //   if (typeof name !== 'string') return;
+  //   return name.trim().replace(/\s+/g, '-').toLowerCase();
+  // };
+
   return (
-    <div>
-      <h1>Family Members</h1>
-      <Notification message={errorMessage} />  
-      <ul>
-        {people.map(person => 
-            <Person
-              key={person.id}
-              person={person}
-            />
-        )}
-      </ul>
-      <h2>Add Family Member</h2>
-        <FamilyMemberForm addPersonToList={setPeople} currentListOfPeople={people} />
+    <Router>
+      <div className='top-nav'>
+        <Link to="/">home</Link>
+        <Link to="/family-tree">Family Tree</Link>
+        <Link to="/family-members">Family Members</Link>
+        <Link to="/add-new-family-member">Add New Family Member</Link>
+        <Link to="/edit-family-member">Edit Family Member</Link>
+      </div>
+
+      <Switch>
+        <Route path='/family-members'>
+          <div>
+            <h1>Family Members</h1>
+            <Notification message={errorMessage} />  
+            <ul>
+              {people.map(person => 
+                  <Person
+                    key={person.id}
+                    person={person}
+                  />
+              )}
+            </ul>
+          </div>
+        </Route>
+        <Route path='/family-tree'>
+          <FamilyTree />
+        </Route>
+        <Route path='/add-new-family-member'>
+          <div>
+            <h2>Add Family Member</h2>
+            <FamilyMemberForm addPersonToList={setPeople} currentListOfPeople={people} />
+          </div>
+        </Route>
+      </Switch>
+
+  
+       
       <Footer />
-    </div>
+
+    </Router>
+    
   );
 };
 
---
+++ src/components/FamilyTree.js
@@ -0,0 +1,109 @@
+import ReactFlow from "react-flow-renderer";
+import { useState } from 'react';
+
+const PEOPLE = [
+  {
+    "name": "Danielle Jones",
+    "id": 1,
+    "parents": ["Megan Brown", "David Quinn"],
+    "partner": [2],
+    "children": [3],
+    "bio": "TBD lolz"
+  },
+  {
+    "name": "Christopher Guzman",
+    "id": 2,
+    "parents": ["Michael Dotson", "Eryn Medina"],
+    "partner": [1],
+    "children": [3],
+    "bio": "blah blah"
+  },
+  {
+    "name": "Stephanie Taylor",
+    "id": 3,
+    "parents": ["Danielle Jones", "Christopher Guzman"],
+    "partner": null,
+    "children": null,
+    "bio": "coolest person ever"
+  }
+];
+
+let defaultElements = [];
+
+function elementsFromTree(text) {
+  const elements = [];
+  const partners = [];
+  let people = PEOPLE;
+  for (let i = 0; i < people.length; i++) {
+    let currentElement = {
+      id: `${people[i].id}`,
+      type: "default",
+      data: { label: `${people[i].name}` },
+      position: { x: i * 100, y: i * 100 }
+    };
+    elements.push(currentElement);
+    // build partnerships
+    if (people[i].partner != null && !partners.flat().includes(people[i].id)) { // instead of null just have empty array
+      let partnerElement = {
+        id: `p${people[i].id}-${people[i].partner[0]}`,
+        type: "default",
+        data: { label: `${people[i].id} to ${people[i].partner[0]} partnership` },
+        position: { x: i * 120, y: i * 120 }
+      };
+      elements.push(partnerElement);
+      partners.push([people[i].id, people[i].partner[0]]);
+    }
+  }
+  
+  // edge creation logic
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].children) { // make sure not an empty array
+      let currentElement = {
+        id: `parent${people[i].id}-${people[i].children}`,
+        type: 'straight',
+        source: `${people[i].id}`,
+        target: `${people[i].children}`
+      };
+      elements.push(currentElement);
+    }
+    if (people[i].partner) { // don't use this! loop through the partners array to make our edges!
+      let currentElement = {
+        id: `partner${people[i].id}-${people[i].partner[0]}`,
+        source: `${people[i].id}`,
+        target: `p${people[i].id}-${people[i].partner[0]}`
+      }
+        elements.push(currentElement);
+    }
+  }
+  return elements;
+}
+
+
+function Nodes() {
+  const [elements, setElements] = useState(defaultElements);
+  return (
+    <div className="pane" id="node-pane">
+      <button
+        onClick={() => {
+          console.log(
+            `Refreshing parser definition and generating graph elements for ${PEOPLE}`
+          );
+          // console.log(JSON.stringify(elementsFromTree(tree, currentEditorContents)));
+          setElements(elementsFromTree(PEOPLE));
+          //console.log(JSON.stringify(elements, 0, 4));
+        }}
+      >
+        Refresh Graph
+      </button>
+      <ReactFlow elements={elements} />
+    </div>
+  );
+}
+
+export function FamilyTree() {
+  return (
+    <div className="pane" id="node-pane">
+      <Nodes></Nodes>
+    </div>
+  );
+}
